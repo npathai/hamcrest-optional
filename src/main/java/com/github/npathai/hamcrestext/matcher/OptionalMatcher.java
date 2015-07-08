1b84eb3366c2fe2175aa7c48772d595ca973f465
Narendra Pathai <narendra.pathai@gmail.com>
2015-07-08 18:12:49 +0530

    Added isPresentAnd matcher and provided proper descriptions of mismatch

--- src/main/java/com/github/npathai/hamcrestext/matcher/OptionalMatcher.java
+++ src/main/java/com/github/npathai/hamcrestext/matcher/OptionalMatcher.java
@@ -4,40 +4,104 @@ import java.util.Optional;
 
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
-import org.hamcrest.TypeSafeDiagnosingMatcher;
+import org.hamcrest.TypeSafeMatcher;
 
-public abstract class OptionalMatcher extends TypeSafeDiagnosingMatcher<Optional<?>> {
+/**
+ * Provides matchers for JDK 8 {@link Optional}
+ * 
+ * @author npathai
+ *
+ */
+public abstract class OptionalMatcher<T> extends TypeSafeMatcher<Optional<T>> {
 
-	public static Matcher<Optional<?>> isPresent() {
-		return new PresenceMatcher();
+	/**
+	 * @return a matcher which matches if Optional is present 
+	 */
+	public static <T> Matcher<Optional<T>> isPresent() {
+		return new PresenceMatcher<T>();
 	}
 
-	private static class PresenceMatcher extends OptionalMatcher {
+	private static class PresenceMatcher<T> extends OptionalMatcher<T> {
 		
 		public void describeTo(Description description) {
-			
+			description.appendText("<Present>");
 		}
 
 		@Override
-		protected boolean matchesSafely(Optional<?> item, Description mismatchDescription) {
+		protected boolean matchesSafely(Optional<T> item) {
 			return item.isPresent();
 		}
+		
+		@Override
+		protected void describeMismatchSafely(Optional<T> item, Description mismatchDescription) {
+			mismatchDescription.appendText("was <Empty>");
+		}
 	}
 
-	public static Matcher<Optional<?>> isAbsent() {
-		return new AbsenceMatcher();
+	/**
+	 * @return a matcher which matches if Optional is empty
+	 */
+	public static <T> Matcher<Optional<T>> isEmpty() {
+		return new EmptyMatcher<T>();
 	}
 	
-	private static class AbsenceMatcher extends OptionalMatcher {
+	private static class EmptyMatcher<T> extends OptionalMatcher<T> {
 
 		public void describeTo(Description description) {
-			
+			description.appendText("<Empty>");
 		}
 
 		@Override
-		protected boolean matchesSafely(Optional<?> item, Description mismatchDescription) {
+		protected boolean matchesSafely(Optional<T> item) {
 			return !item.isPresent();
 		}
 		
+		@Override
+		protected void describeMismatchSafely(Optional<T> item, Description mismatchDescription) {
+			mismatchDescription.appendText("was <Present> with value " + item.get());
+		}
+	}
+
+	/**
+	 * @return a matcher which matches if Optional is present and also matches 
+	 * provided {@code matcher}
+	 */
+	public static <T> Matcher<Optional<T>> isPresentAnd(Matcher<? super T> matcher) {
+		return new PresentAndMatcher<>(matcher);
+	}
+	
+	private static class PresentAndMatcher<T> extends OptionalMatcher<T> {
+		private PresenceMatcher<T> presenceMatcher = new PresenceMatcher<>();
+		private Matcher<? super T> matcher;
+
+		public PresentAndMatcher(Matcher<? super T> matcher) {
+			this.matcher = matcher;
+		}
+		
+		@Override
+		public void describeTo(Description description) {
+			presenceMatcher.describeTo(description);
+			description.appendText(" and ");
+			matcher.describeTo(description);
+		}
+
+		@Override
+		protected boolean matchesSafely(Optional<T> item) {
+			return presenceMatcher.matchesSafely(item) 
+					&& matcher.matches(item.get());
+		}
+		
+		@Override
+		//FIXME presently evaluating matcher twice need to find solution to this
+		protected void describeMismatchSafely(Optional<T> item, Description mismatchDescription) {
+			if (!presenceMatcher.matchesSafely(item)) {
+				mismatchDescription.appendText("was <Empty>");
+			} else {
+				if (!matcher.matches(item.get())) {
+					mismatchDescription.appendText("was <Present> and ");
+					matcher.describeMismatch(item.get(), mismatchDescription);
+				}
+			}
+		}
 	}
 }
--- src/test/java/com/github/npathai/hamcrestext/matcher/OptionalMatcherTest.java
+++ src/test/java/com/github/npathai/hamcrestext/matcher/OptionalMatcherTest.java
@@ -1,7 +1,10 @@
 package com.github.npathai.hamcrestext.matcher;
 
-import static com.github.npathai.hamcrestext.matcher.OptionalMatcher.isAbsent;
-import static com.github.npathai.hamcrestext.matcher.OptionalMatcher.isPresent;
+import static com.github.npathai.hamcrestext.matcher.OptionalMatcher.isEmpty;
+import static com.github.npathai.hamcrestext.matcher.OptionalMatcher.*;
+import static org.hamcrest.CoreMatchers.allOf;
+import static org.hamcrest.CoreMatchers.endsWith;
+import static org.hamcrest.CoreMatchers.startsWith;
 import static org.junit.Assert.assertThat;
 
 import java.util.Optional;
@@ -19,6 +22,8 @@ public class OptionalMatcherTest {
 	@Test
 	public void testIsPresent_ShouldReturnAMatcher_WhichFailsIfOptionalIsEmpty() {
 		exception.expect(AssertionError.class);
+		exception.expectMessage("Expected: <Present>");
+		exception.expectMessage("was <Empty>");
 		
 		Optional<Object> myOptionalRef = Optional.empty();
 		
@@ -33,15 +38,45 @@ public class OptionalMatcherTest {
 	}
 	
 	@Test
-	public void testIsAbsent_ShouldReturnAMatcher_WhichSucceedsIsOptionalIsAbsent() {
-		assertThat(Optional.empty(), isAbsent());
+	public void testIsEmpty_ShouldReturnAMatcher_WhichSucceedsIsOptionalIsEmpty() {
+		Optional<Object> myOptionalRef = Optional.empty();
+		
+		assertThat(myOptionalRef, isEmpty());
 	}
 	
 	@Test
-	public void testIsAbsent_ShouldReturnAMatcher_WhichFailsIfOptionalIsPresent() {
+	public void testIsEmpty_ShouldReturnAMatcher_WhichFailsIfOptionalIsPresent() {
 		exception.expect(AssertionError.class);
+		exception.expectMessage("Expected: <Empty>");
+		exception.expectMessage("was <Present> with value " + ANY_VALUE);
+		
 		Optional<Object> myOptionalRef = Optional.of(ANY_VALUE);
 		
-		assertThat("An optional with some value should not be absent", myOptionalRef, isAbsent());
+		assertThat("An optional with some value should not be empty", myOptionalRef, isEmpty());
+	}
+	
+	@Test
+	public void testIsPresentAnd_ShouldReturnAMatcher_WhichFailsIfOptionalIsEmpty() {
+		exception.expect(AssertionError.class);
+		exception.expectMessage("was <Empty>");
+		
+		Optional<String> hello = Optional.empty();
+		assertThat(hello, isPresentAnd(startsWith("a")));
+	}
+	
+	@Test
+	public void testIsPresentAnd_ShouldReturnAMatcher_WhichSucceedsIfOptionalIsPresent_AndPassedMatcher_Succeeds() {
+		Optional<String> hello = Optional.of("hello");
+		assertThat(hello, isPresentAnd(allOf(startsWith("h"), endsWith("o"))));
+	}
+	
+	@Test
+	public void testIsPresentAnd_ShouldReturnAMatcher_WhichFailsIfOptionalIsPresent_ButPassedMatcher_Fails() {
+		exception.expect(AssertionError.class);
+		exception.expectMessage("was <Present> and");
+		exception.expectMessage("was \"hello\"");
+		
+		Optional<String> hello = Optional.of("hello");
+		assertThat(hello, isPresentAnd(startsWith("a")));
 	}
 }
